0799e061a58583bddb13ef6e62e8131a44ec6218
Samuel Baquero <sabame360@hotmail.com>
2015-10-27 17:04:06 -0500

    Commit Inicial
    Commit inicial de la version 3D del juegoo

---
+++ PMC3D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager gm;
+    public GameObject player;
+    private ArrayList inventory;
+    public GameObject mainCanvas;
+    public GameObject initialMenu;
+    public GameObject inGameMenu;
+
+	// Use this for initialization
+	void Start () {
+        if (player == null)
+        {
+            GameObject.FindWithTag("Player");
+        }
+        //Player Inventory.
+        inventory = new ArrayList();
+        //Turn off mainCanvas, initialize Main Menu
+        mainCanvas.;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
